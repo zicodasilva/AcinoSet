950d77e397494e6ae69a95e94c280252e9be9c32
Zico <zicods7@gmail.com>
2021-04-26 08:57:45 +0100

    Reverted app module (no longer required becuase PyPy will not be used).

--- src/lib/app.py
+++ src/lib/app.py
@@ -17,8 +17,8 @@ from .calib import calibrate_camera, calibrate_fisheye_camera, \
     triangulate_points, triangulate_points_fisheye, \
     project_points, project_points_fisheye, \
     _calibrate_pairwise_extrinsics
-# from .plotting import plot_calib_board, plot_optimized_states, \
-#     plot_extrinsics, Cheetah
+from .plotting import plot_calib_board, plot_optimized_states, \
+    plot_extrinsics, Cheetah
 
 
 def extract_corners_from_images(img_dir, out_fpath, board_shape, board_edge_len, window_size=11, remove_unused_images=False):
@@ -91,51 +91,51 @@ def sba_points_fisheye(scene_fpath, points_2d_df):
 
 # ==========  PLOTTING  ==========
 
-# def plot_corners(points_fpath):
-#     points, fnames, board_shape, board_edge_len, cam_res = load_points(points_fpath)
-#     plot_calib_board(points, board_shape, cam_res)
+def plot_corners(points_fpath):
+    points, fnames, board_shape, board_edge_len, cam_res = load_points(points_fpath)
+    plot_calib_board(points, board_shape, cam_res)
 
 
-# def plot_points_standard_undistort(points_fpath, camera_fpath):
-#     k, d, cam_res = load_camera(camera_fpath)
-#     points, _, board_shape, *_ = load_points(points_fpath)
-#     undistort_pts = create_undistort_point_function(k, d)
-#     undistorted_points = undistort_pts(points).reshape(points.shape)
-#     plot_calib_board(undistorted_points, board_shape, cam_res)
+def plot_points_standard_undistort(points_fpath, camera_fpath):
+    k, d, cam_res = load_camera(camera_fpath)
+    points, _, board_shape, *_ = load_points(points_fpath)
+    undistort_pts = create_undistort_point_function(k, d)
+    undistorted_points = undistort_pts(points).reshape(points.shape)
+    plot_calib_board(undistorted_points, board_shape, cam_res)
 
 
-# def plot_points_fisheye_undistort(points_fpath, camera_fpath):
-#     k, d, cam_res = load_camera(camera_fpath)
-#     points, _, board_shape, *_ = load_points(points_fpath)
-#     undistort_pts = create_undistort_fisheye_point_function(k, d)
-#     undistorted_points = undistort_pts(points).reshape(points.shape)
-#     plot_calib_board(undistorted_points, board_shape, cam_res)
+def plot_points_fisheye_undistort(points_fpath, camera_fpath):
+    k, d, cam_res = load_camera(camera_fpath)
+    points, _, board_shape, *_ = load_points(points_fpath)
+    undistort_pts = create_undistort_fisheye_point_function(k, d)
+    undistorted_points = undistort_pts(points).reshape(points.shape)
+    plot_calib_board(undistorted_points, board_shape, cam_res)
 
 
-# def plot_scene(data_dir, scene_fname=None, manual_points_only=False, **kwargs):
-#     *_, scene_fpath = find_scene_file(data_dir, scene_fname, verbose=False)
-#     points_dir = os.path.join(os.path.dirname(scene_fpath), "points")
-#     pts_2d, frames = [], []
-#     if manual_points_only:
-#         points_fpaths = os.path.join(points_dir, "manual_points.json")
-#         pts_2d, frames, _ = load_manual_points(points_fpaths)
-#         pts_2d = pts_2d.swapaxes(0, 1)
-#         frames = [frames]*len(pts_2d)
-#     else:
-#         points_fpaths = sorted(glob(os.path.join(points_dir, 'points[1-9].json')))
-#         for fpath in points_fpaths:
-#             img_pts, img_names, *_ = load_points(fpath)
-#             pts_2d.append(img_pts)
-#             frames.append(img_names)
+def plot_scene(data_dir, scene_fname=None, manual_points_only=False, **kwargs):
+    *_, scene_fpath = find_scene_file(data_dir, scene_fname, verbose=False)
+    points_dir = os.path.join(os.path.dirname(scene_fpath), "points")
+    pts_2d, frames = [], []
+    if manual_points_only:
+        points_fpaths = os.path.join(points_dir, "manual_points.json")
+        pts_2d, frames, _ = load_manual_points(points_fpaths)
+        pts_2d = pts_2d.swapaxes(0, 1)
+        frames = [frames]*len(pts_2d)
+    else:
+        points_fpaths = sorted(glob(os.path.join(points_dir, 'points[1-9].json')))
+        for fpath in points_fpaths:
+            img_pts, img_names, *_ = load_points(fpath)
+            pts_2d.append(img_pts)
+            frames.append(img_names)
 
-#     plot_extrinsics(scene_fpath, pts_2d, frames, triangulate_points_fisheye, manual_points_only, **kwargs)
+    plot_extrinsics(scene_fpath, pts_2d, frames, triangulate_points_fisheye, manual_points_only, **kwargs)
 
 
-# def plot_cheetah_states(states, smoothed_states=None, out_fpath=None, mplstyle_fpath=None):
-#     fig, axs = plot_optimized_states(states, smoothed_states, mplstyle_fpath)
-#     if out_fpath is not None:
-#         fig.savefig(out_fpath, transparent=True)
-#         print(f'Saved {out_fpath}\n')
+def plot_cheetah_states(states, smoothed_states=None, out_fpath=None, mplstyle_fpath=None):
+    fig, axs = plot_optimized_states(states, smoothed_states, mplstyle_fpath)
+    if out_fpath is not None:
+        fig.savefig(out_fpath, transparent=True)
+        print(f'Saved {out_fpath}\n')
 
 
 def _plot_cheetah_reconstruction(positions, data_dir, scene_fname=None, labels=None, **kwargs):
